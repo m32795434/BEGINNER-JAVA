c755edb0f706b4b10d05e0a15ebc0961073b26c2
Manuel Augusto Bravard <manubravard@gmail.com>
2022-10-07 11:32:56 -0300

    numeric systems

--- src/NumericSystems.java
+++ src/NumericSystems.java
@@ -1,12 +1,17 @@
 public class NumericSystems {
     public static void main(String[] args) {
-
-        int decimalNumber = 500;
-        System.out.println("numero binario de " + decimalNumber + " = "+ Integer.toBinaryString(decimalNumber));
-int intFromBinaryNunber = 0b111110100;//0bnumeroBinario > its integer equivalent. Otherwise, it would be 111,110,100
-        System.out.println("intFromBinaryNunber = " + intFromBinaryNunber);
-        System.out.println("numero octal de " + decimalNumber + " = "+ Integer.toOctalString(decimalNumber));
-int intFromOctalNumber = 0764;
+        int decimal = 500;
+        System.out.println("decimal = " + decimal);
+        System.out.println("binaryStringVersionOfDecimal = " + Integer.toBinaryString(decimal));
+        int intFromBinaryNumber = 0b111110100;
+        System.out.println("intFromBinaryNumber = " + intFromBinaryNumber);
+        int intFromBinaryString = Integer.parseInt((Integer.toBinaryString(decimal)), 2);
+        System.out.println("intFromBinaryString = " + intFromBinaryString);
+        System.out.println("octalStringVersionOfDecimal = " + Integer.toOctalString(decimal));
+        int  intFromOctalNumber = 0764;
         System.out.println("intFromOctalNumber = " + intFromOctalNumber);
+        System.out.println("hexaStringVersionOfDecimal =" + Integer.toHexString(decimal));
+        int intFromHexaNumber = 0x1f4;
+        System.out.println("intFromHexaNumber = " + intFromHexaNumber);
     }
 }
