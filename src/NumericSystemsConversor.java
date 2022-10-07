a558502054fa44a5f4006e2af3b92b4339280552
Manuel Augusto Bravard <manubravard@gmail.com>
2022-10-07 16:04:50 -0300

    numeric system converter ready

--- src/NumericSystemsConversor.java
+++ src/NumericSystemsConversor.java
@@ -3,9 +3,17 @@ import javax.swing.*;
 public class NumericSystemsConversor {
     public static void main(String[] args) {
         String stringFormatResponse = JOptionPane.showInputDialog(null, "insert an integer number");
-        int dialogResponse = Integer.parseInt(stringFormatResponse, 10);
-        System.out.println("numberResponse = " + dialogResponse);
-        String report = "Your number \n in binary is : " + Integer.toBinaryString(dialogResponse) + "\n in octa: " + Integer.toOctalString(dialogResponse) + "\n in hexa: " + Integer.toHexString(dialogResponse);
+        int intDialogResponse = 0;
+        try{
+        intDialogResponse = Integer.parseInt(stringFormatResponse, 10);
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "ERROR: please insert a valid number!");
+            System.out.println(e);
+            main(args);
+            System.exit(0);//with this, we kill the program. We could "return;" too. But exit is more consistent.
+        }
+        System.out.println("numberResponse = " + intDialogResponse);
+        String report = "Your number: "+ intDialogResponse + "\n in binary is : " + Integer.toBinaryString(intDialogResponse) + "\n in octa: " + Integer.toOctalString(intDialogResponse) + "\n in hexa: " + Integer.toHexString(intDialogResponse);
         JOptionPane.showMessageDialog(null, report);
     }
 }
