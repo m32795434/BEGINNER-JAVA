d12ef8fb30b2b7532bb3cc6466f905b55dbffe00
Manuel Augusto Bravard <manubravard@gmail.com>
2022-10-11 20:58:01 -0300

    types conversion!with input dialog and try/catch

---
+++ src/TypesConversion.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class TypesConversion {
+    public static void main(String[] args) {
+       String stringFormatResponse = JOptionPane.showInputDialog(null, "Please insert a decimal number");
+        boolean boolVersion = false;
+        int intVersion = 0;
+        double realDoubleVersion = 0;
+       try{
+           boolVersion = Boolean.parseBoolean(stringFormatResponse);
+           intVersion = Integer.parseInt(stringFormatResponse);
+           realDoubleVersion = Double.parseDouble(stringFormatResponse);
+    }catch(NumberFormatException e){
+           if(e.getMessage() == "Cannot parse null string"){
+               System.exit(0);
+           }
+           JOptionPane.showMessageDialog(null, "Neee, invalid!Try again!");
+           main(args);
+System.exit(0);
+    }
+
+JOptionPane.showMessageDialog(null, "your vale: " + stringFormatResponse + "\n" +"boolVersion = " + boolVersion + "\n"+ "intVersion = " + intVersion +"\n"+ "realDoubleVersion = " + realDoubleVersion);
+    }
+}
