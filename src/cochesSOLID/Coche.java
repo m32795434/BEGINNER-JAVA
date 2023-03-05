763a45cedeb89b1462634dcec685aa5c3fdd71ce
Manuel Augusto Bravard <manubravard@gmail.com>
2023-03-04 22:56:44 -0300

    Principio de Abierto - Cerrado

--- src/cochesSOLID/Coche.java
+++ src/cochesSOLID/Coche.java
@@ -1,6 +1,9 @@
 package cochesSOLID;
 
 //Principio de Abierto - Cerrado
+//se define una interfaz común para un conjunto de algoritmos relacionados y se implementan varias clases que
+// encapsulan diferentes implementaciones de estos algoritmos.
+//Audi, Renault, harán uso de estos métodos descriptos abajo, a su manera..y así lo pueden hacer futuras clases
 public abstract class Coche {
 protected String marca;
 abstract int precioMedioCoche();
